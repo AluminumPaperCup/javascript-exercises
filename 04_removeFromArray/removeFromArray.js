ef870320052b43ead507c4e4b8fb9713d60c01fa
Brandon <104651603+AluminumPaperCup@users.noreply.github.com>
2024-07-11 16:36:45 -0400

    Complete Exercises have all passed using filter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(ray, ...newArray) {
+        return ray.filter(val => !newArray.includes(val))
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
