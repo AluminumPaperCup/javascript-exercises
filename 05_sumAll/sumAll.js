8aef19e62efadf7c65a63dbe66253c7d5e8f4b6b
Brandon <104651603+AluminumPaperCup@users.noreply.github.com>
2024-07-11 19:07:39 -0400

    Complete all 6 tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(small, large) {
+    
+//condition check if inputs are not integers
+    if (small > large) [small, large] = [large, small];
 
+    if (small < 0 || large < 0) 
+        return "ERROR";
+
+    if (!Number.isInteger(small) || !Number.isInteger(large)) return "ERROR";
+
+// Runs loop for total sum
+    let finalSum = 0;
+    for (let i = small; i <= large; i++) {
+       finalSum += i;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
